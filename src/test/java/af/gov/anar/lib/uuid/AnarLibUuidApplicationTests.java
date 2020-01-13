f8ce1c33e299383223fb0a1e540f44fcfb0e75a8
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 12:54:18 +0430

    uuid library

---
+++ src/test/java/af/gov/anar/lib/uuid/AnarLibUuidApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.uuid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibUuidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
